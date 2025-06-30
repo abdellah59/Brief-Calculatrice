c3d7c353e494040d29487cf546c5b17741511e1c
abdellah59 <abdellah.ait59@gmail.com>
2025-06-30 15:37:07 +0200

    feat(program.cs): add while loop to do another calculation after and validate input for division with 0

--- Brief-Calculatrice/Program.cs
+++ Brief-Calculatrice/Program.cs
@@ -7,8 +7,8 @@
 
             double nbr1 = 0;
             double nbr2 = 0;
-            double resultat = 0;
 
+            // fonctions des operation arithmétiques réutilisables 
             double Additionner(double nbr1, double nbr2)
             {
                 double resultat = nbr1 + nbr2;
@@ -39,56 +39,90 @@
 
             }
 
-            Console.WriteLine("---------- Calculatrice ----------");
+            // 
+            bool continuercalcul = true;
 
-            Console.Write("Saisisez un nombre : ");
-            nbr1 = double.Parse(Console.ReadLine());
-
-            Console.Write("Saisisez un nombre : ");
-            nbr2 = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("Choix des options de la calculatrice");
-            Console.WriteLine("+ : Addition");
-            Console.WriteLine("- : Soustraction");
-            Console.WriteLine("* : Multiplication");
-            Console.WriteLine("/ : Division");
-            Console.WriteLine("esc : Quitter l'application calclutratrice");
-            Console.Write("Option choisie : ");
-
-
-            switch (Console.ReadLine())
+            while (continuercalcul)
             {
-                case "+":
-                    Console.Clear();
-                   
-                    Console.WriteLine($"Resultat : {nbr1} + {nbr2} = " + Additionner(nbr1,nbr2));
-                    break;
 
-                case "-":
-                    Console.Clear();
-                    Console.WriteLine($"Resultat : {nbr1} + {nbr2} = " + Soustraire(nbr1, nbr2));
-                    break;
+                Console.WriteLine("---------- Calculatrice ----------");
 
-                case "*":
-                    Console.Clear();
-                    Console.WriteLine($"Resultat : {nbr1} * {nbr2} = " + Multiplier(nbr1, nbr2));
-                    break;
+                Console.Write("Saisisez un nombre : ");
+                double.TryParse(Console.ReadLine(), out nbr1);
 
-                case "/":
-                    Console.Clear();
-                    Console.WriteLine($"Resultat : {nbr1} / {nbr2} = " + Diviser(nbr1, nbr2));
-                    break;
+                Console.Write("Saisisez un nombre : ");
+                double.TryParse(Console.ReadLine(), out nbr2);
 
-                case "esc":
-                    Console.Clear();
-                    Console.WriteLine("Au revoir !");
-                    Environment.Exit(0);
+                Console.WriteLine("Choix des options de la calculatrice");
+                Console.WriteLine("+ : Addition");
+                Console.WriteLine("- : Soustraction");
+                Console.WriteLine("* : Multiplication");
+                Console.WriteLine("/ : Division");
+                Console.WriteLine("esc : Quitter l'application calclutratrice");
+                Console.Write("Option choisie : ");
+
+
+                 
+                 // switch loop which allows arithmetic operations to be performed depending on the choice of operator
+                
+
+                switch (Console.ReadLine())
+                {
+                    case "+":
+                        Console.Clear();
+
+                        Console.WriteLine($"Resultat : {nbr1} + {nbr2} = " + Additionner(nbr1, nbr2));
+                        break;
+
+                    case "-":
+                        Console.Clear();
+                        Console.WriteLine($"Resultat : {nbr1} + {nbr2} = " + Soustraire(nbr1, nbr2));
+                        break;
+
+                    case "*":
+                        Console.Clear();
+                        Console.WriteLine($"Resultat : {nbr1} * {nbr2} = " + Multiplier(nbr1, nbr2));
+                        break;
+
+                    case "/":
+                        while (nbr2 == 0)
+                        {
+                            
+                            Console.WriteLine("Erreur !! saisissez un nombre autre que 0 : ");
+                            double.TryParse(Console.ReadLine(), out nbr2);
+
+                        }
+                        Console.Clear();
+                        Console.WriteLine($"Resultat : {nbr1} / {nbr2} = " + Diviser(nbr1, nbr2));
+                        break;
+
+                    case "esc":
+                        Console.Clear();
+                        Console.WriteLine("Au revoir !");
+                        Environment.Exit(0);
+                        break;
+
+
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Choix invalide ! Veuillez saisir +, -, *, / ou esc.");
+                        break;
+
+
+                }
+
+                Console.ReadLine();
+                Console.Write("Vous voulez faire d'autres operations (y/n) : ");
+                string reponse = Console.ReadLine();
+
+                if (reponse != "y")
+                {
+                    Console.WriteLine("Au revoir !!");
                     break;
+                }
+                Console.WriteLine();
 
             }
-
-            Console.Write("Appuyez sur une touche pour fermer la calculatrice ......");
-            Console.ReadKey();
 
         }
     }
