92c5ac083fcd2560c57de8091888dc9652b8086f
abdellah59 <abdellah.ait59@gmail.com>
2025-06-30 11:34:01 +0200

    feat(add calculator operation choices with a switch loop

--- Brief-Calculatrice/Program.cs
+++ Brief-Calculatrice/Program.cs
@@ -12,16 +12,53 @@
             double resultat = 0;
 
             Console.Write("Saisisez un nombre : ");
-            nbr1 = Convert.ToInt32(Console.ReadLine);
+            nbr1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Saisisez un nombre");
-            nbr2 = Convert.ToInt32(Console.ReadLine);
+            Console.Write("Saisisez un nombre : ");
+            nbr2 = Convert.ToInt32(Console.ReadLine());
+            
 
             Console.WriteLine("Choix des options de la calculatrice");
-            Console.WriteLine("\+ : Addition");
-            Console.WriteLine("\- : Soustraction");
-            Console.WriteLine("\* : Multiplication");
-           
+            Console.WriteLine("+ : Addition");
+            Console.WriteLine("- : Soustraction");
+            Console.WriteLine("* : Multiplication");
+            Console.WriteLine("/ : Division");
+            Console.WriteLine("esc : Quitter l'application calclutratrice");
+            Console.Write("Option choisie : ");
+
+
+            switch (Console.ReadLine())
+            {
+                case "+":
+                    Console.Clear();
+                    Console.WriteLine($"Resultat : {nbr1} + {nbr2} = " + (nbr1 + nbr2));
+                    break;
+
+                case "-":
+                    Console.Clear();
+                    Console.WriteLine($"Resultat : {nbr1} + {nbr2} = " + (nbr1 - nbr2));
+                    break;
+
+                case "*":
+                    Console.Clear();
+                    Console.WriteLine($"Resultat : {nbr1} + {nbr2} = " + (nbr1 * nbr2));
+                    break;
+
+                case "/":
+                    Console.Clear();
+                    Console.WriteLine($"Resultat : {nbr1} + {nbr2} = " + (nbr1 / nbr2));
+                    break;
+
+                case "esc":
+                    Console.Clear();
+                    Console.WriteLine("Au revoir !");
+                    Environment.Exit(0);
+                    break;
+
+            }
+
+            Console.Write("Appuyez sur une touche pour fermer la calculatrice ......");
+            Console.ReadKey();
 
         }
     }
