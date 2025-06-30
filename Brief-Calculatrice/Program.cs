325711bf14398bb4f8ae36f13d11f39cded1cb76
abdellah59 <abdellah.ait59@gmail.com>
2025-06-30 10:57:20 +0200

    feat(program.cs) :  initialize the calculator with the declaration of variables and the choice of options

--- Brief-Calculatrice/Program.cs
+++ Brief-Calculatrice/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("---------- Calculatrice ----------");
+
+            int nbr1 = 0;
+            int nbr2 = 0;
+            double resultat = 0;
+
+            Console.Write("Saisisez un nombre : ");
+            nbr1 = Convert.ToInt32(Console.ReadLine);
+
+            Console.Write("Saisisez un nombre");
+            nbr2 = Convert.ToInt32(Console.ReadLine);
+
+            Console.WriteLine("Choix des options de la calculatrice");
+            Console.WriteLine("\+ : Addition");
+            Console.WriteLine("\- : Soustraction");
+            Console.WriteLine("\* : Multiplication");
+           
+
         }
     }
 }
